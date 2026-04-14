81691d2faca1e7cda97769f0413b9a22fb81b701
PatriciaChips <leeroymckellar@gmail.com>
2026-04-14 14:38:19 +1000

    - Merged - Bloodweaver completed
    Took 18 hours 22 minutes

--- src/main/java/org/pat/abilities/Objects/Affinity.java
+++ src/main/java/org/pat/abilities/Objects/Affinity.java
@@ -1,10 +1,22 @@
 package org.pat.abilities.Objects;
 
+import org.pat.pattyEssentialsV3.Utils;
+
 public enum Affinity {
-    movement(),
-    passive(),
-    combat(),
-    support();
+    movement(Utils.lightblue + "Movement"),
+    passive(Utils.green + "Passive"),
+    combat(Utils.darkred + "Combative"),
+    support(Utils.lightred + "Support");
+
+    private String text;
+
+    Affinity(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
 
     /**
      * Can be expanded to include affinity descriptions or an associated colour/s for example
